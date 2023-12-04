d99a96bfdfdf943fd781a3b0ea3401f85430f10e
SharvaniNadagouda <sharvaninadagouda@gmail.com>
2023-12-04 20:41:01 +0530

    Signup and Login Done

---
+++ src/main/java/Controller/login.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Service.MyService;
+@WebServlet("/login")
+public class login extends HttpServlet
+{
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		MyService service = new MyService();
+		service.login(req, resp);
+
+}
+}
